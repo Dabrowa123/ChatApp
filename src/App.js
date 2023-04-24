063f9e3d04b5618acde730f7d947cb93e59d1897
SDabrowa <dabrowa.szymon@gmail.com>
2023-04-24 17:46:18 +0200

    adding admin panel with protected route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import AdminPanel from "./components/AdminPanel";
+import Protected from "./components/Protected";
 
 function App() {
+
+  const isLoggedIn = false;
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route exact path="/" Component={Login}></Route>
+          <Route exact path="/adminpanel" element={
+            <Protected isLoggedIn={isLoggedIn}> 
+              <AdminPanel />
+            </Protected>
+          }></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/components/AdminPanel.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const AdminPanel = () => {
+
+  return <div>Admin Panel</div>;
+};
+
+export default AdminPanel;
