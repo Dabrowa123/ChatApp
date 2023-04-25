f3128c2a270f3fb4cd3a0b2d3f1807308135a8f3
SDabrowa <dabrowa.szymon@gmail.com>
2023-04-25 13:12:00 +0200

    removing unecessary code and comments

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
-import React, { useState, useSyncExternalStore } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
@@ -9,19 +9,14 @@ import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 import { Container } from "@mui/material";
-import FormControl from "@mui/material/FormControl";
-import { InputLabel, Input, Button } from "@mui/material";
-import { logInUser } from "../actions/logInUser";
-import { useEffect } from "react";
+import { Button } from "@mui/material";
 import { useNavigate } from "react-router";
 
 const Login = () => {
   const navigate = useNavigate();
   const [usernameValue, setUsernameValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
-  const displayIsLogged = useSelector((state) => state.isLogged);
   const users = useSelector((state) => state.users);
-  console.log("Komponent Login: " + displayIsLogged);
 
   const authenticate = () => {
     const filteredUser = users.filter((user) => user.userName == usernameValue);
@@ -64,35 +59,6 @@ const Login = () => {
   };
 
   return (
-    // <div>
-    //   <form onSubmit={onHandleSubmit}>
-    //     <div>
-    //       <InputLabel htmlFor="my-input">
-    //         <Input
-    //           id="my-input"
-    //           name="username"
-    //           aria-describedby="my-helper-text"
-    //           value={usernameValue}
-    //           onChange={onEditHandle}
-    //         />
-    //       </InputLabel>
-    //     </div>
-    //     <div>
-    //       <InputLabel htmlFor="my-input">
-    //         <Input
-    //           id="my-input"
-    //           name="password"
-    //           aria-describedby="my-helper-text"
-    //           value={passwordValue}
-    //           onChange={onEditHandle}
-    //         />
-    //       </InputLabel>
-    //     </div>
-    //     <button>Zaloguj się</button>
-    //   </form>
-    //   <br />
-    //   <button onClick={handleLogin}>Login Admin01</button>
-    // </div>
     <Container component="main" maxWidth="sm">
       <Box
         sx={{
