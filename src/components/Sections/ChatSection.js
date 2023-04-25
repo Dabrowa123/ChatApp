733e9a6dbbc3d16179b21adf49cfab96e31fa5b0
SDabrowa <dabrowa.szymon@gmail.com>
2023-04-25 16:04:16 +0200

    adding sections to userpanel

---
+++ src/components/Sections/ChatSection.js
@@ -0,0 +1,7 @@
+import Box from "@mui/material/Box";
+
+const ChatSection = () => {
+  return <Box sx={{ bgcolor: "#fafafa", height: "100vh", width: "50vw" }} />;
+};
+
+export default ChatSection;
---
+++ src/components/Sections/UsersSection.js
@@ -0,0 +1,15 @@
+import Box from "@mui/material/Box";
+
+const UsersSection = () => {
+  return (
+    <Box
+      sx={{
+        // background: "linear-gradient(to right bottom, #36EAEF, #6B0AC9)",
+        height: "100vh",
+        width: "25vw",
+      }}
+    />
+  );
+};
+
+export default UsersSection;
