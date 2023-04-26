f7b230228f0ded3399256d847c8e6273571a5ad4
GreenMush02 <domino280702@gmail.com>
2023-04-26 17:24:24 +0200

    Folders, userSection and everything is good

---
+++ src/components/Sections/ChatGroupsSection/ChatGroupCreator.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createGroup } from "../../../actions/groupActions/createGroup";
+
+const ChatGroupCreator = () => {
+  const dispatch = useDispatch();
+  const [groupNameValue, setGroupNameValue] = useState("");
+
+  const onEditHandle = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === "groupName") {
+      setGroupNameValue(value);
+    }
+  };
+
+  const createRoom = (e) => {
+    e.preventDefault();
+    dispatch(createGroup(groupNameValue));
+    setGroupNameValue("");
+  };
+  return (
+    <div>
+      <form onSubmit={createRoom}>
+        <label>
+          Nazwa pokoju:
+          <input
+            type="text"
+            name="groupName"
+            value={groupNameValue}
+            onChange={onEditHandle}
+          ></input>
+        </label>
+        <button>Dodaj pokój</button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatGroupCreator;
