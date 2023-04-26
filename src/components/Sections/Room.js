de7911777f23a038d9194ca5531ba320939eb74e
GreenMush02 <domino280702@gmail.com>
2023-04-26 11:11:49 +0200

    UserPanel has working groupList

--- src/components/Sections/Room.js
+++ src/components/Sections/Room.js
@@ -4,25 +4,6 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 
-const Room = () => {
-  return (
-    <Typography variant="h1" fontSize="large">
-      <ListItemButton>
-        <ListItemIcon>
-          <GroupsRoundedIcon
-            // fontSize="large"
-            color="primary"
-            sx={{
-              background: "InfoBackground",
-              padding: "5px",
-              borderRadius: "12px",
-            }}
-          />
-        </ListItemIcon>
-        <ListItemText primary="Room Name" />
-      </ListItemButton>
-    </Typography>
-  );
-};
+const Room = () => {};
 
 export default Room;
