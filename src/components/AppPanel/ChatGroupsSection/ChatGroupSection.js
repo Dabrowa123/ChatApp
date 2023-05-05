485332570ea37a7f912041085123620a6ab2d01b
SDabrowa <dabrowa.szymon@gmail.com>
2023-05-05 10:45:18 +0200

    refactoring logo

--- src/components/AppPanel/ChatGroupsSection/ChatGroupSection.js
+++ src/components/AppPanel/ChatGroupsSection/ChatGroupSection.js
@@ -1,12 +1,11 @@
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import ChatIcon from "@mui/icons-material/Chat";
 import Divider from "@mui/material/Divider";
 import { Typography } from "@mui/material";
 import ChatGroupList from "./ChatGroupList";
 import ChatGroupCreator from "./ChatGroupCreator";
 import { useSelector } from "react-redux";
 import ChatSearch from "./ChatSearch";
+import ChatGroupLogo from "./ChatGroupLogo";
 
 const ChatGroupSection = () => {
   const displayUsers = useSelector((state) => state.users);
@@ -24,32 +23,21 @@ const ChatGroupSection = () => {
         minWidth: "25vw",
         maxWidth: "400px",
         display: "flex",
-        flexDirection: "column"
+        flexDirection: "column",
       }}
     >
-        <Stack
-          justifyContent={"center"}
-          alignItems={"center"}
-          direction={"row"}
-          spacing={2}
-          sx={{ minHeight: "150px" }}
-        >
-          <ChatIcon
-            sx={{ color: "white", minWidth: "80px", minHeight: "100px" }}
-          />
-          <Typography variant="h4">LOGO</Typography>
-        </Stack>
-        <Divider />
-          <Typography variant="h6" textAlign={"left"} mt={3} mb={2} ml={5}>
-            GROUPS
-          </Typography>
-          <Box sx={{ flexGrow: "4", overflow: "auto" }}>
-            <ChatGroupList />
-          </Box>
-          <Box pb={3} pl={2}>
-            <ChatSearch />
-            {isAdmin && <ChatGroupCreator />}
-          </Box>
+      <ChatGroupLogo />
+      <Divider />
+      <Typography variant="h6" textAlign={"left"} mt={3} mb={2} ml={5}>
+        GROUPS
+      </Typography>
+      <Box sx={{ flexGrow: "4", overflow: "auto" }}>
+        <ChatGroupList />
+      </Box>
+      <Box pb={3} pl={2}>
+        <ChatSearch />
+        {isAdmin && <ChatGroupCreator />}
+      </Box>
     </Box>
   );
 };
